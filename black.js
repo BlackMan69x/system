f558d7e8f32b8cbbc87fe9a5d9e62a90445e7a0f
BlackMan <78313191+xBlackMan69@users.noreply.github.com>
2021-07-31 20:59:06 +0300

    Update black.js

--- black.js
+++ black.js
@@ -1156,6 +1156,32 @@ client.on("message", async message => {
   }
 });
 
+client.on("message", message => {
+  if (message.content === prefix + "lockall") {
+    if (!message.member.hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send("**Please Check Your Permission**");
+    message.guild.channels.cache.forEach(c => {
+      c.createOverwrite(message.guild.id, {
+        SEND_MESSAGES: false
+      });
+    });
+    message.channel.send("**✅Done Locked Total Channels On Server!**");
+  }
+});
+
+client.on("message", message => {
+  if (message.content === prefix + "unlockall") {
+    if (!message.member.hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send("**Please Check Your Permission**");
+    message.guild.channels.cache.forEach(c => {
+      c.createOverwrite(message.guild.id, {
+        SEND_MESSAGES: true
+      });
+    });
+    message.channel.send("**✅Done Unlocked Total Channels On Server**");
+  }
+});
+
 
 client.on("message", message => {
   if (message.content.startsWith(prefix + "move")) {
